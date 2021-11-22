b1f2d853a45e665c3eea0c236f077f8f54c6ca7c
VoidMD <mkm4620@hotmail.com>
2021-11-22 15:24:02 +0300

    File implementation

--- src/main.java
+++ src/main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+import java.io.*;
+
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        String FilePath;
+        Scanner input = new Scanner(System.in);
+        Scanner FileRead = null;
+
+            System.out.println("please enter file path :");
+
+                FilePath = input.nextLine();
+
+                FileRead = new Scanner(new FileInputStream(new File(FilePath)));
+
+                while (FileRead.hasNext()){
+                    System.out.println(FileRead.next());
+                }
+
 
     }
+
+    public void Encryption(){
+
+    }
+
+    public void Decryption(){
+
+    }
+
 }
