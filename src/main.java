5a8b2d435bbbd930d3929ccc79aa63f6829d63ba
VoidMD <mkm4620@hotmail.com>
2021-11-26 16:16:09 +0300

    MenuChoice

--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.*;
 import java.io.*;
 
@@ -8,6 +9,29 @@ public class main {
         Scanner FileRead = null;
         int e= 0;
         long n= 0;
+        boolean flag = true;
+
+        do {
+            try {
+                switch (getMenuChoice()) {
+                    case 1:
+
+                        break;
+
+                    case 2:
+
+                        break;
+
+                    case 3:
+                        System.out.println("Terminating ... ");
+                        System.exit(0);
+                        break;
+                }
+            }catch(Exception e){
+                System.out.println(e);
+            }
+        } while (flag) ;
+
 
             System.out.println("please enter file path :");
 
@@ -36,6 +60,24 @@ public class main {
 
     public void Decryption(){
 
+    }
+
+    public static  int  getMenuChoice(){
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do{
+            System.out.println("Please select the operation: ");
+            System.out.println("1. Add to front");
+            System.out.println("2. Add to rear");
+            System.out.println("3. Exit");
+
+            choice = scanner.nextInt();
+            if(choice < 1 || choice > 3)
+                System.out.println("Error: Wrong operation!");
+        }while(choice < 1 || choice > 3);
+
+        return choice;
     }
 
 }
