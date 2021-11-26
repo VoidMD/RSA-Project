61e31bdd9a6b869bf8a80a38f2916acca4fa7383
VoidMD <mkm4620@hotmail.com>
2021-11-26 14:27:31 +0300

    File implementation

--- src/main.java
+++ src/main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 import java.io.*;
 
 public class main {
@@ -6,16 +6,26 @@ public class main {
         String FilePath;
         Scanner input = new Scanner(System.in);
         Scanner FileRead = null;
+        int e= 0;
+        long n= 0;
 
             System.out.println("please enter file path :");
 
                 FilePath = input.nextLine();
 
-                FileRead = new Scanner(new FileInputStream(new File(FilePath)));
+                FileRead = new Scanner(new FileInputStream(new File(FilePath+".txt")));
 
-                while (FileRead.hasNext()){
-                    System.out.println(FileRead.next());
-                }
+                    e = FileRead.nextInt();
+                    n = FileRead.nextLong();
+                    FileRead.nextLine();
+
+                    while (FileRead.hasNext()){
+                        System.out.println(FileRead.nextLine());
+                    }
+
+
+                System.out.println(e);
+                System.out.println(n);
 
 
     }
